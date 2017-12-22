d6ecf0cdf5328f3a933c72289533fed86ccdc9a3
Mirmik <mirmikns@yandex.ru>
2017-12-22 17:25:34 +0300

    a

--- lines.py
+++ lines.py
@@ -3,18 +3,21 @@
 
 from sympy import *
 
-x1, y1, z1, o1, p1, s1, t1, x2, y2, z2, o2, p2, s2, t2 = symbols("x1, y1, z1, o1, p1, s1, t1, x2, y2, z2, o2, p2, s2, t2")
+x12, y12, z12, x1, y1, z1, o1, p1, s1, t1, x2, y2, z2, o2, p2, s2, t2 = symbols("x12, y12, z12, x1, y1, z1, o1, p1, s1, t1, x2, y2, z2, o2, p2, s2, t2")
 
 s = sqrt(
-		((x1 + o1 * t1) - (o2 * t2))**2 +
-		((y1 + p1 * t1) - (p2 * t2))**2 +
-		((z1 + s1 * t1) - (s2 * t2))**2
+		((x12 + o1 * t1) - (o2 * t2))**2 +
+		((y12 + p1 * t1) - (p2 * t2))**2 +
+		((z12 + s1 * t1) - (s2 * t2))**2
 	)
 
 d1 = diff(s,t1)
 d2 = diff(s,t2)
 
 slv = solve((d1,d2, Eq(o1**2+p1**2+s1**2,1)),(t1,t2))
+
+sss = slv.subs(o1**2+p2**2+s2**2, 1)
+sss = sss.subs([(x12, x1 - x2), (y12, y1 - y2), (z12, z1 - z2)])
 
 print(s)
 print()
@@ -23,3 +26,6 @@ print()
 print(d2)
 print()
 print(slv)
+print()
+
+print(sss)
