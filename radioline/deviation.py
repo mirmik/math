022caa288dfd07dc7cb040b48af381d0e6a393bc
mirmik <mirmikns@yandex.ru>
2018-06-06 17:52:50 +0300

    a

--- radioline/deviation.py
+++ radioline/deviation.py
@@ -2,14 +2,14 @@
 #coding: utf-8
 
 #deviation = 0x9CC 
-deviation = - 0xFF_FF_FF_FF + 0xFF_FF_E2DD
+deviation = - 0xFF_FF_FF_FF + 0xFF_FE_7D90
 #dist = 9000000
-dist = -6000000
+dist = -56780
 
-#deviation = 100_000
+#deviation = -100_000
 
-cdiv = 1749
-ndiv = cdiv * (dist/(dist + deviation))
+cdiv = 5000
+ndiv = cdiv / (dist/(dist + deviation))
 
 print("error: {}".format(deviation))
 print("distance: {}".format(dist))
