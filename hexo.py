b27ceb9b6ee39599f3074a4de2679de92d5479df
mirmik <mirmikns@yandex.ru>
2019-01-22 16:40:10 +0300

    't'

--- hexo.py
+++ hexo.py
@@ -8,7 +8,7 @@ lazy = evalcache.Lazy(".evalcache")
 
 solve = lazy(solve)
 
-var("alpha beta q(0:3) l0 l1 l2 z r")
+var("alpha beta q(0:3) l0 l1 l2 z r x y z")
 
 #M1 = Matrix([[cos(q1),-sin(q1)],[sin(q1),cos(q1)]])
 #M2 = Matrix([[cos(q1),-sin(q1)],[sin(q1),cos(q1)]])
@@ -29,7 +29,15 @@ slv1 = slv[1]
 slv.unlazy()
 slv0 = slv[0]
 
-pprint(slv0.unlazy())
+h = sqrt(x**2 + y**2)
+
+#pprint(slv0.unlazy()[0].subs({beta:atan2(z, h), r:sqrt(z**2+h**2)}))
+#pprint(slv0.unlazy()[1].subs({beta:atan2(z, h), r:sqrt(z**2+h**2)}))
+#print()
+pprint(Eq(q0, atan2(y,x)))
+pprint(Eq(q1, slv0.unlazy()[0].subs({beta:atan2(z, h), r:sqrt(z**2+h**2)})))
+pprint(Eq(q2, slv0.unlazy()[1].subs({beta:atan2(z, h), r:sqrt(z**2+h**2)})))
+
 #pprint(slv0.unlazy())
 #pprint(slv1.unlazy())
 #pprint(slv0[0].unlazy())
